dd78e18854e347b315ee47589ebd56572d5159c5
dogancankilment <dogancankilment@gmail.com>
2018-02-23 00:26:54 +0300

    paginator disabled

--- intanco/views.py
+++ intanco/views.py
@@ -16,22 +16,20 @@ def post(request, post_id):
 
 def home(request):
     blog_list = Post.objects.all() # get(is_verified="False")
-    paginator = Paginator(blog_list, 3)
     page = request.GET.get('page')
 
-    try:
-        post_count = paginator.page(page)
-
-    except PageNotAnInteger:
-        # If page is not an integer, deliver first page.
-        post_count = paginator.page(1)
-
-    except EmptyPage:
-        # If page is out of range (e.g. 9999), deliver last page of results.
-        post_count = paginator.page(paginator.num_pages)
+    # try:
+    #     post_count = paginator.page(page)
+    #
+    # except PageNotAnInteger:
+    #     # If page is not an integer, deliver first page.
+    #     post_count = paginator.page(1)
+    #
+    # except EmptyPage:
+    #     # If page is out of range (e.g. 9999), deliver last page of results.
+    #     post_count = paginator.page(paginator.num_pages)
 
     return render_to_response("home/index.html",
                               {"blogs": blog_list,
                                "request": request,
-                               "post_count": post_count,
                                "read_more": 1})
