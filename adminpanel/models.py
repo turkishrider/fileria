cfe679db6da8b0586418b6d04bc0b2a60f09c8a3
dogancankilment <dogancankilment@gmail.com>
2018-02-22 04:23:12 +0300

    Post model created for post objects

--- adminpanel/models.py
+++ adminpanel/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    category = models.CharField(max_length=100)
+    tags = models.TextField()
+    which_user = models.ForeignKey(User)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    image = models.ImageField(upload_to='blog_posts/',
+                              default='blog_posts/default.jpg')
+    is_verified = models.BooleanField(default=False)
+
+
+    class Meta:
+        ordering = ['-updated_at']
