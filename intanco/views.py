b85603d8908b554d32f23f88db76c62fa7eb6c14
dogancankilment <dogancankilment@gmail.com>
2018-02-22 04:35:02 +0300

    home page function created for all blog posts

--- intanco/views.py
+++ intanco/views.py
@@ -12,3 +12,26 @@ def post(request, post_id):
     return render(request, "home/single-post.html",
                               {'post': root_post,
                                'request': request})
+
+
+def home(request):
+    blog_list = Post.objects.all() # get(is_verified="False")
+    paginator = Paginator(blog_list, 3)
+    page = request.GET.get('page')
+
+    try:
+        post_count = paginator.page(page)
+
+    except PageNotAnInteger:
+        # If page is not an integer, deliver first page.
+        post_count = paginator.page(1)
+
+    except EmptyPage:
+        # If page is out of range (e.g. 9999), deliver last page of results.
+        post_count = paginator.page(paginator.num_pages)
+
+    return render_to_response("home/index.html",
+                              {"blogs": blog_list,
+                               "request": request,
+                               "post_count": post_count,
+                               "read_more": 1})
