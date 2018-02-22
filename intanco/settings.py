9ee7a3cdcc34f50da0654d1367e683be3d97ac65
dogancankilment <dogancankilment@gmail.com>
2018-02-22 03:50:03 +0300

    project_path and project_root variables defined for database, templates, static files, media files.

--- intanco/settings.py
+++ intanco/settings.py
@@ -1,7 +1,13 @@
 # Django settings for intanco project.
 
+import os
+from os import path
+
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
+
+PROJECT_ROOT = path.dirname(path.abspath(path.dirname(__file__)))
+PROJECT_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir))
 
 ADMINS = (
     # ('Your Name', 'your_email@example.com'),
@@ -11,8 +17,8 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': path.join(PROJECT_ROOT, 'db.sqlite3'), # 'var/www/intanco/db.sqlite3'
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
