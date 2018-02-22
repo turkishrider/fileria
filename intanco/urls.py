cfba144b24356cec00bb6f445c12ea9f3d42a9bf
dogancankilment <dogancankilment@gmail.com>
2018-02-22 04:26:22 +0300

    home urls, admin-panel app urls, authentication app urls created

--- intanco/urls.py
+++ intanco/urls.py
@@ -1,17 +1,31 @@
 from django.conf.urls import patterns, include, url
+from django.conf import settings
 
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'intanco.views.home', name='home'),
-    # url(r'^intanco/', include('intanco.foo.urls')),
+                       url(r'^$', 'newsportal.views.home', name='home'),
+                       url(r'^post/(?P<post_id>\w+)$', 'newsportal.views.post', name='post'),
 
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+                       url(r'^admin-panel$', 'adminpanel.views.home', name='adminhome'),
+                       url(r'^create-post$', 'adminpanel.views.createpost', name='createpost'),
+                       url(r'^edit-post/(?P<post_id>\w+)$', 'adminpanel.views.editpost', name='editpost'),
+                       url(r'^show-post/(?P<post_id>\w+)$', 'adminpanel.views.single_post', name='show_post'),
 
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
-)
+                       url(r'^login', 'authentication.views.login', name='login'),
+                       url(r'^register', 'authentication.views.register', name='register'),
+                       url(r'^logout$', 'authentication.views.logout', name='logout'),
+                       url(r'^activation/(?P<token_id>.*)', 'authentication.views.activation', name='activation'),
+
+                       )
+
+if settings.DEBUG:
+    # static files (images, css, javascript, etc.)
+    urlpatterns += patterns('',
+                            (r'^media/(?P<path>.*)$',
+                             'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}))
+
+if settings.DEBUG:   # if DEBUG is True it will be served automatically
+    urlpatterns += patterns('',
+                            url(r'^static/(?P<path>.*)$',
+                                'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
+                            )
